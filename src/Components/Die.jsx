40c31b37cb2fe650b8d1ffc690eb0303be205d5e
“Claudio” <claudio.marino@outlook.com>
2024-07-21 22:37:24 +0200

    see the images

--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,14 +1,14 @@
 export default function Die(props) {
+	function getImageUrl(name) {
+		return new URL(`/src/assets/dice/${name}.svg`, import.meta.url).href;
+	}
+
 	const styles = {
 		backgroundColor: props.isHeld ? "#59E391" : "white",
 	};
 	return (
 		<div className="die-face" style={styles} onClick={props.holdDice}>
-			<img
-				className="die-num"
-				src={`/src/assets/dice/${props.value}.svg`}
-				alt="die"
-			/>
+			<img className="die-num" src={getImageUrl(props.value)} alt="die" />
 		</div>
 	);
 }
